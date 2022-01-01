2a888842efd31244321bd35feb54ee99cd8293f7
NNrepos <nivimano@gmail.com>
2022-01-01 11:49:57 +0200

    clean url files gitignore pycharm migrate sql add default unity assets unity is running - but not in html :)

--- Django/DjangoUnityTutorial/DjangoUnityTutorial/urls.py
+++ Django/DjangoUnityTutorial/DjangoUnityTutorial/urls.py
@@ -2,8 +2,7 @@
 Definition of urls for DjangoUnityTutorial.
 """
 
-from datetime import datetime
-from django.conf.urls import patterns, url, include
+from django.conf.urls import patterns, url
 from rest_framework import routers
 
 router = routers.DefaultRouter(trailing_slash=False)
@@ -11,18 +10,19 @@ router = routers.DefaultRouter(trailing_slash=False)
 # Uncomment the next lines to enable the admin:
 from django.conf.urls import include
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    #url(r'^$', 'app.views.home', name='home'),
-    url(r'^', include('unitybackendapp.urls')),
+                       # Examples:
+                       # url(r'^$', 'app.views.home', name='home'),
+                       url(r'^', include('unitybackendapp.urls')),
 
-    url(r'^api/', include(router.urls)),
+                       url(r'^api/', include(router.urls)),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-     url(r'^admin/', include(admin.site.urls)),
-)
+                       # Uncomment the next line to enable the admin:
+                       url(r'^admin/', include(admin.site.urls)),
+                       )
